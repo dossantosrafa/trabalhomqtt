65f785443e80ecce9ef7dfb7018c6be4cd0e2602
dossantosrafa <rafaelfontana256@gmail.com>
2026-06-03 20:14:47 -0300

    adiciona config.example.h

---
+++ include/config.example.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// Copie este arquivo para src/config.h e preencha com seus dados
+
+#define WIFI_SSID     "sua_rede"
+#define WIFI_PASSWORD "sua_senha"
+
+#define MQTT_HOST     "endereco_do_broker"
+#define MQTT_PORT     1884
+#define MQTT_USUARIO  "seu_usuario"
+#define MQTT_SENHA    "sua_senha"
+
+#define TOPICO_COMANDO   "casa/led"
+#define TOPICO_STATUS    "casa/nodemcu/status"
+#define TOPICO_IP        "casa/nodemcu/ip"
+#define TOPICO_UPTIME    "casa/nodemcu/uptime"
+#define TOPICO_CONFIRMA  "casa/led/confirmacao"
